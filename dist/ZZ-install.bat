a36f65c718a2e1ff95119633028da4ce62ccad53
Robin <robinvandernoord@gmail.com>
2020-08-01 15:03:32 +0200

    Update ZZ-install.bat

--- dist/ZZ-install.bat
+++ dist/ZZ-install.bat
@@ -3,6 +3,8 @@
 @echo off
 set TARGET='%~dp0\script\script.exe'
 set SHORTCUT='%userprofile%\Start Menu\Programs\Startup\script.lnk'
+: TODO: add flag to use 'shell:common startup' folder instead of user specific one
+
 set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
 
 %PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.TargetPath = %TARGET%; $S.Save()"
